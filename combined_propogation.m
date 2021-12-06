6439a5d742906d631ca77bd485a45de61796eef0
Prince John <prince.apple.john@gmail.com>
2021-12-06 00:56:26 -0600

    fixed combined matrix

--- combined_propogation.m
+++ combined_propogation.m
@@ -1,10 +1,10 @@
 function rays_out = combined_propogation(d1, d2, f, rays)
 
 
-M = [1-d2/f  d1+d2-(d1*d2)*f 0 0;
-    -1/f 1-d1/f 0 0;
-    0 0 1-d2/f d1+d2-(d1*d2)/f; 
-    0 0 -1/f 1-d1/f];
+M = [-(d2/d1) 0 0 0;
+    -1/f -(d1/d2) 0 0;
+    0 0 -(d2/d1) 0; 
+    0 0 -1/f -(d1/d2)];
 
 rays_out = M*rays;
 
